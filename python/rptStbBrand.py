181095ae2214359e9fdfda8c77a173202cceedad
kai-peel <kai@peel.com>
2015-04-06 11:26:54 -0700

    IR-158
    https://peel-jira.atlassian.net/browse/IR-158

---
+++ python/rptStbBrand.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+__author__ = 'kai'
+import os
+from utils import irutils
+
+LOGFILE = 'C:/Users/G50/Dropbox/Public/list_of_brands_and_codeset_count.csv'
+
+
+def report(cnx, log):
+    try:
+        query = ("select a.brandid, a.brandname, c.country, c.displayname, count(*) "
+                 "from brands a, codesets b, brandnames c "
+                 "where a.brandid=b.brandid and a.brandid=c.brandid and devicetypeid=2 and activeflag='Y' "
+                 "group by a.brandid, country order by brandname; ")
+        cnx.cursor.execute(query)
+        r = cnx.cursor.fetchall()
+        for each in r:
+            log.write('%s,%s,%s,%s,%d\n' % (each[0], each[1], each[2], each[3], each[4]))
+
+    except Exception, e:
+        print 'report: %s' % e
+
+
+def main():
+    try:
+        os.remove(LOGFILE)
+    except OSError:
+        pass
+    with open(LOGFILE, 'w') as f:
+        cnx = irutils.DBConnection()
+        f.write('BrandID,BrandName,CountryCode,Localization,CodesetCount\n')
+        report(cnx, f)
+        f.close()
+
+if __name__ == '__main__':
+    main()
